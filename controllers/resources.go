cdcf6df395f150cd5a7c563332b27f60e0dcf90b
林坤 <linkun>
2022-06-24 14:47:33 +0800

    update: 瘦身workload_controller

---
+++ controllers/resources.go
@@ -0,0 +1,122 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	workloadsv1alpha1 "github.com/zisefeizhu/workload-operator/api/v1alpha1"
+	"github.com/zisefeizhu/workload-operator/controllers/template"
+	"github.com/zisefeizhu/workload-operator/controllers/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"reflect"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// 集中处理 部署组
+func (r *WorkloadReconciler) deploymentGroup(instance *workloadsv1alpha1.Workload, ctx context.Context, req ctrl.Request) (*workloadsv1alpha1.DeploymentGroupStatus, error) {
+	// 获取工作负载模版
+	w := template.NewWorkload(instance).Template()
+	if err := controllerutil.SetControllerReference(instance, w.(metav1.Object), r.Scheme); err != nil {
+		return nil, err
+	}
+	found := template.NewWorkload(instance).Found()
+	if err := r.Get(ctx, req.NamespacedName, found.(client.Object)); err != nil {
+		if errors.IsNotFound(err) {
+			err := r.Create(ctx, w.(client.Object))
+			if err != nil {
+				r.Logger.Error(err, "create app failed")
+				return nil, err
+			}
+			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", instance.Spec.WorkloadSpec.Type), fmt.Sprintf("type is %s name is %s create in  %s namespace", instance.Spec.WorkloadSpec.Type, instance.Name, instance.Namespace))
+		}
+		if !errors.IsNotFound(err) {
+			return nil, err
+		}
+	} else {
+		if err := r.Update(ctx, w.(client.Object)); err != nil {
+			r.Logger.Error(err, "update app failed")
+			return nil, err
+		}
+		//r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", instance.Spec.Type), fmt.Sprintf("type is %s name is %s  update in %s namespace", instance.Spec.Type, instance.Name, instance.Namespace))
+	}
+	// todo
+	// 处理工作负载的status
+	// 类型断言
+	return utils.WorkloadStatusProcessor(w), nil
+}
+
+// 处理svc的 func
+func (r *WorkloadReconciler) svc(instance *workloadsv1alpha1.Workload, ctx context.Context) (*workloadsv1alpha1.ServiceStatus, error) {
+	// service的处理
+	service := template.NewService(instance)
+	err := controllerutil.SetControllerReference(instance, service, r.Scheme)
+	if err != nil {
+		return nil, err
+	}
+	s := &corev1.Service{}
+	if err := r.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, s); err != nil {
+		if errors.IsNotFound(err) && instance.Spec.SvcSpec.EnableService {
+			if err := r.Create(ctx, service); err != nil {
+				r.Logger.Error(err, "create service failed")
+				return nil, err
+			}
+			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s create in  %s namespace", "serivce", instance.Name, instance.Namespace))
+		}
+		if !errors.IsNotFound(err) && instance.Spec.SvcSpec.EnableService {
+			return nil, err
+		}
+	} else {
+		if instance.Spec.SvcSpec.EnableService {
+			currIP := s.Spec.ClusterIP
+			s.Spec.ClusterIP = currIP
+			service.Spec.ClusterIP = currIP
+			if !reflect.DeepEqual(s.Spec, service.Spec) {
+				s.Spec = service.Spec
+				if err := r.Update(ctx, s); err != nil {
+					r.Logger.Error(err, "update service failed")
+					return nil, err
+				}
+				r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s update in  %s namespace", "serivce", instance.Name, instance.Namespace))
+			}
+		} else {
+			if err := r.Delete(ctx, s); err != nil {
+				return nil, err
+			}
+			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s delete in  %s namespace", "serivce", instance.Name, instance.Namespace))
+		}
+	}
+	return &workloadsv1alpha1.ServiceStatus{
+		ServiceIP: s.Spec.ClusterIP,
+	}, nil
+}
+
+// 处理wk status的 func
+func (r *WorkloadReconciler) workloadStatus(instance *workloadsv1alpha1.Workload, dgStatus *workloadsv1alpha1.DeploymentGroupStatus, svcStatus *workloadsv1alpha1.ServiceStatus, ctx context.Context) error {
+	// status
+	s := workloadsv1alpha1.WorkloadStatus{}
+	s.DeploymentGroupStatus = *dgStatus
+	s.ServiceStatus = *svcStatus
+	// 更改cr的状态
+	// 计算工作负载和svc
+	if *dgStatus.Replicas == 0 {
+		s.Phase = workloadsv1alpha1.PendingPhase
+	} else if *dgStatus.Replicas == dgStatus.AvailableReplicas {
+		s.Phase = workloadsv1alpha1.RunningPhase
+	} else if *dgStatus.Replicas != dgStatus.AvailableReplicas {
+		s.Phase = workloadsv1alpha1.UpdatePhase
+	}
+	instance.Status = s
+	// todo
+	return r.Status().Update(ctx, instance)
+
+}
+
+// 处理wk phase的 func
+func (r *WorkloadReconciler) workloadPhase(ctx context.Context, instance *workloadsv1alpha1.Workload, phase workloadsv1alpha1.Phase) error {
+	instance.Status.Phase = phase
+	return r.Status().Update(ctx, instance)
+}
--- controllers/workload_controller.go
+++ controllers/workload_controller.go
@@ -18,21 +18,13 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-logr/logr"
 	workloadsv1alpha1 "github.com/zisefeizhu/workload-operator/api/v1alpha1"
-	"github.com/zisefeizhu/workload-operator/controllers/template"
-	"github.com/zisefeizhu/workload-operator/utils"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
-	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"time"
 )
 
@@ -63,15 +55,18 @@ type WorkloadReconciler struct {
 /*
   1、svc 先于 工作负载创建
   2、工作负载适配:deployment";"statefulSet";"daemonSet";"job";"cronJob
-  3、 目前尚未对任务重新入队列进行处理
 */
 
 func (r *WorkloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	//logger := log.FromContext(ctx)
+	forget := reconcile.Result{}
+	requeue := ctrl.Result{
+		RequeueAfter: time.Second * 2,
+	}
 	_ = r.Logger.WithValues("workloads", req.NamespacedName)
 	instance := &workloadsv1alpha1.Workload{}
 	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+		return forget, client.IgnoreNotFound(err)
 	}
 
 	// svc 处理逻辑
@@ -79,9 +74,9 @@ func (r *WorkloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	if err != nil {
 		er := r.workloadPhase(ctx, instance, workloadsv1alpha1.FailedPhase)
 		if er != nil {
-			return ctrl.Result{}, er
+			return requeue, er
 		}
-		return ctrl.Result{}, err
+		return requeue, err
 	}
 
 	// 工作负载 处理逻辑
@@ -91,16 +86,17 @@ func (r *WorkloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		if er != nil {
 			return ctrl.Result{}, er
 		}
-		return ctrl.Result{}, err
+		return requeue, err
 	}
 
 	// wk 的 status 处理
 	err = r.workloadStatus(instance, dgStatus, svcStatus, ctx)
 	if err != nil {
-		return ctrl.Result{}, err
+		return forget, err
 	}
+	// 根据workload 的status 对比spec, 进行矫正
 
-	return ctrl.Result{RequeueAfter: 2 * time.Second}, nil
+	return requeue, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -108,109 +104,4 @@ func (r *WorkloadReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&workloadsv1alpha1.Workload{}).
 		Complete(r)
-}
-
-// 集中处理 部署组
-func (r *WorkloadReconciler) deploymentGroup(instance *workloadsv1alpha1.Workload, ctx context.Context, req ctrl.Request) (*workloadsv1alpha1.DeploymentGroupStatus, error) {
-	// 获取工作负载模版
-	w := template.NewWorkload(instance).Template()
-	if err := controllerutil.SetControllerReference(instance, w.(metav1.Object), r.Scheme); err != nil {
-		return nil, err
-	}
-	found := template.NewWorkload(instance).Found()
-	if err := r.Get(ctx, req.NamespacedName, found.(client.Object)); err != nil {
-		if errors.IsNotFound(err) {
-			err := r.Create(ctx, w.(client.Object))
-			if err != nil {
-				r.Logger.Error(err, "create app failed")
-				return nil, err
-			}
-			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", instance.Spec.WorkloadSpec.Type), fmt.Sprintf("type is %s name is %s create in  %s namespace", instance.Spec.WorkloadSpec.Type, instance.Name, instance.Namespace))
-		}
-		if !errors.IsNotFound(err) {
-			return nil, err
-		}
-	} else {
-		if err := r.Update(ctx, w.(client.Object)); err != nil {
-			r.Logger.Error(err, "update app failed")
-			return nil, err
-		}
-		//r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", instance.Spec.Type), fmt.Sprintf("type is %s name is %s  update in %s namespace", instance.Spec.Type, instance.Name, instance.Namespace))
-	}
-	// todo
-	// 处理工作负载的status
-	// 类型断言
-	return utils.WorkloadStatusProcessor(w), nil
-}
-
-// 处理svc的 func
-func (r *WorkloadReconciler) svc(instance *workloadsv1alpha1.Workload, ctx context.Context) (*workloadsv1alpha1.ServiceStatus, error) {
-	// service的处理
-	service := template.NewService(instance)
-	err := controllerutil.SetControllerReference(instance, service, r.Scheme)
-	if err != nil {
-		return nil, err
-	}
-	s := &corev1.Service{}
-	if err := r.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, s); err != nil {
-		if errors.IsNotFound(err) && instance.Spec.SvcSpec.EnableService {
-			if err := r.Create(ctx, service); err != nil {
-				r.Logger.Error(err, "create service failed")
-				return nil, err
-			}
-			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s create in  %s namespace", "serivce", instance.Name, instance.Namespace))
-		}
-		if !errors.IsNotFound(err) && instance.Spec.SvcSpec.EnableService {
-			return nil, err
-		}
-	} else {
-		if instance.Spec.SvcSpec.EnableService {
-			currIP := s.Spec.ClusterIP
-			s.Spec.ClusterIP = currIP
-			service.Spec.ClusterIP = currIP
-			if !reflect.DeepEqual(s.Spec, service.Spec) {
-				s.Spec = service.Spec
-				if err := r.Update(ctx, s); err != nil {
-					r.Logger.Error(err, "update service failed")
-					return nil, err
-				}
-				r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s update in  %s namespace", "serivce", instance.Name, instance.Namespace))
-			}
-		} else {
-			if err := r.Delete(ctx, s); err != nil {
-				return nil, err
-			}
-			r.Recorder.Event(instance, corev1.EventTypeNormal, fmt.Sprintf("%s-controller", "service"), fmt.Sprintf("type is %s name is %s delete in  %s namespace", "serivce", instance.Name, instance.Namespace))
-		}
-	}
-	return &workloadsv1alpha1.ServiceStatus{
-		ServiceIP: s.Spec.ClusterIP,
-	}, nil
-}
-
-// 处理wk status的 func
-func (r *WorkloadReconciler) workloadStatus(instance *workloadsv1alpha1.Workload, dgStatus *workloadsv1alpha1.DeploymentGroupStatus, svcStatus *workloadsv1alpha1.ServiceStatus, ctx context.Context) error {
-	// status
-	s := workloadsv1alpha1.WorkloadStatus{}
-	s.DeploymentGroupStatus = *dgStatus
-	s.ServiceStatus = *svcStatus
-	// 更改cr的状态
-	// 计算工作负载和svc
-	if *dgStatus.Replicas == 0 {
-		s.Phase = workloadsv1alpha1.PendingPhase
-	} else if *dgStatus.Replicas == dgStatus.AvailableReplicas {
-		s.Phase = workloadsv1alpha1.RunningPhase
-	} else if *dgStatus.Replicas != dgStatus.AvailableReplicas {
-		s.Phase = workloadsv1alpha1.UpdatePhase
-	}
-	instance.Status = s
-	// todo
-	return r.Status().Update(ctx, instance)
-
-}
-
-// 处理wk phase的 func
-func (r *WorkloadReconciler) workloadPhase(ctx context.Context, instance *workloadsv1alpha1.Workload, phase workloadsv1alpha1.Phase) error {
-	instance.Status.Phase = phase
-	return r.Status().Update(ctx, instance)
 }
